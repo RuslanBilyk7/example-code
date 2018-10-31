9f7a8ac38c4d58035d1cd0ac938af3156f15c80d
RUSLAN77 <ruslan01234567890123@gmail.com>
2018-10-31 13:30:25 +0200

    Initial commit

---
+++ springidol-javaconfig/src/main/java/com/springinaction/springidol/Main.java
@@ -0,0 +1,12 @@
+//package com.springinaction.springidol;
+//
+///**
+// * Created by RUSLAN77 on  23.05.2018 in Ukraine
+//ПРОБА ПЕРА)))))))))))
+// */
+//public class Main {
+//    public static void main(String[] args) {
+//        Instrumentalist instrumentalist= new Instrumentalist();
+//        instrumentalist.getClass();
+//    }
+//}
